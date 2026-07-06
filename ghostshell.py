ffd60426408a64f7ef98f03b688c0eb1c0e4788e
Hertty15 <prasham9dec@example.com>
2026-07-06 23:05:32 +0530

    it can open simple websites and execute basic commands

--- ghostshell.py
+++ ghostshell.py
@@ -1,24 +1,57 @@
 import subprocess
+import os
+import webbrowser
 
 def main():
-    print("Welcome to GhostShell! (Type 'exit' to quit.)")
+    shell_name="GhostShell"
+    print(f"Welcome to {shell_name}!(Type 'help' to see commands, 'exit' to quit)")
 
-    #keeps the shell running
     while True:
-        #show promt
-        user_input = input("GhostShell> ")
-        #check if user wants to exit
-        if user_input.lower()=='exit':
-            print("Exiting GhostShell. Goodbye!")
+        user_input=input(f"{shell_name}> ")
+        #this cuts sentence into words
+        #eg: "open youtube" becomes ["open", "youtube"]
+        parts=user_input.split()
+
+        #if u press enter without anything else do nothing
+        if not parts:
+            continue
+
+        command=parts[0].lower()#first word like "open" or "exit"
+        args=" ".join(parts[1:])#rest of the words
+
+        #CUSTOM COMMANDS
+        if command=="exit":
+            print(f"Exiting {shell_name}...")
             break
 
-        #execute the command
-        try:
-            #shell=true tells python to pass your text to windows
-            subprocess.run(user_input, shell=True)
-        except Exception as e:
-            print(f"Error occurred: {e}")
+        elif command=="clear":
+            os.system('cls')#clear the entire screen (for windows)
 
-#this tells python to run the main function when the script is executed
+        elif command=="help":
+            print("Available commands:")
+            print("  open <url> - Open a URL in the default web browser")
+            print("  clear - Clear the screen")
+            print("  exit - Exit the shell")
+            print("--------------------------------------------")
+            print("You can also run any system command directly, like 'dir' or 'calc'.")
+
+        elif command=="open" and args:
+            #if u type open youtube it adds .com (try to make it smarter)
+            if"." not in args:
+                args+=".com"
+            if not args.startswith("http"):
+                args="http://"+args
+            
+            webbrowser.open(args)
+            print(f"Opening {args} in the default web browser...")
+
+        #WINDOWS PASS THROUGH
+        else:
+            #if its not custom command let windwos handle it
+            try:
+                subprocess.run(user_input, shell=True)
+            except Exception as e:
+                print(f"Error executing command: {e}")
+
 if __name__ == "__main__":
     main()
